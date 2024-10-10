e1d2ecdf6f8f30fb34fcf5a4889d9d7b52ede8f6
Maximo Machado <maximo@mit.edu>
2024-10-09 22:32:32 -0700

    Use node instead of control for CardView

--- scenes/card_view/card_view.gd
+++ scenes/card_view/card_view.gd
@@ -1,34 +1,26 @@
-extends Container
+extends Node2D
 
 var startPosition: Vector2
 var card_highlighted: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var card_front := $CardFront
+	var container := $Container
+	var card_front := $CardTemplate
 	var card_front_size : Vector2 = card_front.texture.get_size()
-	custom_minimum_size = Vector2(card_front_size.x / card_front.hframes, \
+	container.custom_minimum_size = Vector2(card_front_size.x / card_front.hframes, \
 									card_front_size.y / card_front.vframes)
-	print(custom_minimum_size)
+	print(container.custom_minimum_size)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-
-func _on_mouse_entered() -> void:
+func _on_container_mouse_entered() -> void:
 	$AnimationPlayer.play("Select")
 	card_highlighted = true
 
 
-func _on_mouse_exited() -> void:
+func _on_container_mouse_exited() -> void:
 	$AnimationPlayer.play("Deselect")
 	card_highlighted = false
-
-
-func _on_gui_input(event: InputEvent) -> void:
-	if (event is InputEventMouseButton):
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			pass
-			
-			
