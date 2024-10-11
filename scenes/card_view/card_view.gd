139632c9f91bdb0615d30afbb60132668ae8baf3
Maximo Machado <maximo@mit.edu>
2024-10-10 17:14:14 -0700

    Remove static function

--- scenes/card_view/card_view.gd
+++ scenes/card_view/card_view.gd
@@ -3,11 +3,6 @@ class_name CardView extends Node2D
 @export var card: Card
 var card_highlighted: bool = false
 
-static func new_card_view(card_state: Card) -> CardView:
-	var card_view: CardView = load("res://scenes/card_view/card_view.tscn").instantiate()
-	card_view.card = card_state
-	return card_view
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var container := $DetectMouse
@@ -18,13 +13,12 @@ func _ready() -> void:
 	print_debug(container.custom_minimum_size)
 	
 	if card is Follower:
-		print_debug(" yaya")
 		$Health.text = str(card.stats.health)
 		$Influence.text = str(card.stats.influence)
 		$Attack.text = str(card.stats.attack)
 	else:
 		print_debug("nono")
-	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
