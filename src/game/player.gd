40b4c990161a4740a3cfe3fa01d18d5bf02708a0
Maximo Machado <maximo@mit.edu>
2024-10-11 19:27:59 -0700

    Add influence and health to player

--- src/game/player.gd
+++ src/game/player.gd
@@ -4,16 +4,30 @@ extends RefCounted
 
 var id: int
 var hand: Array[Card]
-var main_deck: Deck
+var deck: Deck
 var influence_deck: Deck
 var graveyard: Deck
 
-var summoned_followers: Array[Follower]
+var followers_in_play: Array[Follower]
+var factories_in_play: Array[Factory]
 
+var health : int:
+	set(value):
+		if value < 0:
+			health = 0
+		else:
+			health = value
+
+var influence : int:
+	set(value):
+		if value < 0:
+			influence = 0
+		else:
+			influence = value
 
 func _init(p_id: int, p_hand: Array[Card] = [], p_main_deck: Deck = Deck.new(), p_influence_deck: Deck = Deck.new()) -> void:
 	id = p_id
 	hand = p_hand
-	main_deck = p_main_deck
+	deck = p_main_deck
 	influence_deck = p_influence_deck
 	graveyard = Deck.new()
