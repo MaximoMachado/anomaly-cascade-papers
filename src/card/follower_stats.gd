466f961a4ea9f81b2975715c605ae10b978407b5
Maximo Machado <maximo@mit.edu>
2024-10-12 23:33:41 -0700

    Fix stats health calculation to be mtg-style

--- src/card/follower_stats.gd
+++ src/card/follower_stats.gd
@@ -18,24 +18,17 @@ extends RefCounted
 ## max_health >= 0
 @export var max_health: int :
 	set(value):
-		var health_change = max_health - value
+		var health_change := value - max_health
 		max_health = maxi(0, value)
-		# Current health increases with positive change but clamps with negative change
-		if health_change > 0:
-			_health += health_change
-		else:
-			_health = clamp(_health, 0, max_health)
 
-
-## Private variable to prevent infinite loop with max_health setter
-var _health: int
+		# Current health changes the same amount as max_health does
+		health += health_change
 
 ## Current health of the follower
 ## 0 <= health <= max_health
 @export var health: int :
-	get: return _health
 	set(value):
-		_health = clamp(value, 0, max_health)
+		health = clamp(value, 0, max_health)
 
 func _init(p_attack: int = 0, p_influence: int = 0, p_max_health: int = 0) -> void:
 	attack = p_attack
