514a7fdb77b2a788d5178c8a9c551912aafbfa6d
Maximo Machado <maximo@mit.edu>
2024-10-18 08:24:39 -0700

    Use state chart instead of hand-rolled state machine

--- scenes/card_view/card_view.gd
+++ scenes/card_view/card_view.gd
@@ -9,7 +9,6 @@ var starting_position: Vector2:
 		
 var hovered_position: Vector2:
 	get: return starting_position + MAXIMUM_HOVER_MOVEMENT
-var drag_offset: Vector2 = Vector2(0, 0)
 
 var size: Vector2:
 	get: return $Background.size
@@ -17,15 +16,10 @@ var size: Vector2:
 @export var ANIMATION_DURATION := 0.2
 @export var MAXIMUM_HOVER_MOVEMENT := Vector2(0, -50)
 @export var card_highlighted: bool = false
-
-# Drag state
-var mouse_colliding = false
-enum DragState { DRAGGING, STATIC }
-var drag_state = DragState.STATIC
 @export var dragging_speed = 1.0
 
 
-# Called when the node enters the scene tree for the first time.
+# Initialize UI information based on card
 func _ready() -> void:
 	card = Follower.new()
 	starting_position = position
@@ -52,42 +46,35 @@ func _ready() -> void:
 		%FollowerStats.hide()
 		push_error("Unhandled card type in CardView")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:	
-	if Input.is_action_pressed("left_click") and mouse_colliding:
-		drag_state = DragState.DRAGGING
-	elif Input.is_action_just_released("left_click"):
-		drag_state = DragState.STATIC
-		drag_offset = Vector2(0, 0)
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", starting_position, ANIMATION_DURATION)
-	
-	match drag_state:
-		DragState.STATIC:
-			pass
-		DragState.DRAGGING:
-			var mouse_position = get_global_mouse_position()
-			position = lerp(position, mouse_position - (size / 2), dragging_speed * delta)
+func _input(event: InputEvent):
+	if Input.is_action_pressed("select"):
+		%StateChart.send_event("selected")
+	elif Input.is_action_just_released("select"):
+		%StateChart.send_event("deselected")
 
 func _on_container_mouse_entered() -> void:
-	print_debug("enter")
-	mouse_colliding = true
-	if drag_state == DragState.STATIC:
-		drag_offset = position - get_global_mouse_position()
-		var scaled_duration = ANIMATION_DURATION
-		
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", hovered_position, scaled_duration)
-		card_highlighted = true
-
+	%StateChart.send_event("mouse_entered")
 
 func _on_container_mouse_exited() -> void:
-	mouse_colliding = false
-	print_debug("exit")
-	if drag_state == DragState.STATIC:
-		var scaled_duration = ANIMATION_DURATION
-		
-		var tween = get_tree().create_tween() 
-		tween.tween_property(self, "position", starting_position, ANIMATION_DURATION)
-		card_highlighted = true
+	%StateChart.send_event("mouse_exited")
+
+func _on_transition_from_dragging_to_static() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", starting_position, ANIMATION_DURATION)
+
+
+func _on_dragging_state_processing(delta: float) -> void:
+	var mouse_position = get_global_mouse_position()
+	position = lerp(position, mouse_position - (size / 2), dragging_speed * delta)
+
+
+func _on_transition_from_static_to_hovered() -> void:	
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", hovered_position, ANIMATION_DURATION)
+	card_highlighted = true
+
+
+func _on_transition_from_hovered_to_static() -> void:
+	var tween = get_tree().create_tween() 
+	tween.tween_property(self, "position", starting_position, ANIMATION_DURATION)
+	card_highlighted = false
