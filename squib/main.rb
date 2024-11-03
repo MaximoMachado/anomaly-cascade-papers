bbc3c22e7b57043839d386369e86ee4080da002c
Maximo Machado <maximo@mit.edu>
2024-11-02 23:45:08 -0700

    Add templates and render out cards based on type

--- squib/main.rb
+++ squib/main.rb
@@ -14,19 +14,27 @@ def combine_text(arr_1=[], arr_2=[], seperator="")
     return arr_1.zip(arr_2).map{ |type, classificaton| classificaton != "" ? type + seperator + classificaton : type}
 end
 
+def card_template(type)
+    if type.include? "Follower"
+        return "images/follower_card_template.svg"
+    else
+        return "images/card_template.svg"
+    end
+end
+
 Squib::Deck.new(width: 825, height: 1125, cards: data['name'].size, layout: 'card.yml') do
   background color: 'white'
   set font: 'Times New Roman,Serif 10.5'
-  hint text: '#333' # show extents of text boxes to demo the layout
 
+  svg file: data['type'].map{ |t| card_template(t) }, layout: :card
   text str: data['name'], layout: :title
   text str: data['flux'], layout: :title_right
   text str: "art", layout: :art
-  svg file: data['art'], placeholder: "images/lab-coat.svg", layout: :art
+  svg file: data['art'], placeholder: "images/lab_coat.svg", layout: :art
   text str: combine_text(data['type'], data['class'], ' - '), layout: :type
   text str: data['desc'], layout: :description
-  text str: data['attack'], layout: :lower_right
-  text str: data['influence'], layout: :lower_middle
-  text str: data['health'], layout: :lower_left
+  text str: data['attack'], layout: :attack
+  text str: data['influence'], layout: :influence
+  text str: data['health'], layout: :health
   save_png prefix: data['name'].map{ |n| 'wsf_' + n.downcase }
 end
