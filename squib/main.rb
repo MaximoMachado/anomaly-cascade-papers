ff6d3e4700bbd96933a2a73934c1958dda4649d9
Maximo Machado <maximo@mit.edu>
2024-11-03 00:36:18 -0700

    Substitute text for the mana icons

--- squib/main.rb
+++ squib/main.rb
@@ -27,12 +27,34 @@ Squib::Deck.new(width: 825, height: 1125, cards: data['name'].size, layout: 'car
   set font: 'Times New Roman,Serif 10.5'
 
   svg file: data['type'].map{ |t| card_template(t) }, layout: :card
+
   text str: data['name'], layout: :title
-  text str: data['flux'], layout: :title_right
+  text(str: data['flux'], layout: :title_right) do | embed |
+    embed.svg key: ':B:', dy: -40, width: 50, height: 50, file: 'images/flux_icons/foundation.svg'
+    embed.svg key: ':R:', dy: -40, width: 50, height: 50, file: 'images/flux_icons/gilded.svg'
+    embed.svg key: ':W:', dy: -40, width: 50, height: 50, file: 'images/flux_icons/veil_walkers.svg'
+    embed.svg key: ':G:', dy: -40, width: 50, height: 50, file: 'images/flux_icons/fey.svg'
+    embed.svg key: ':P:', dy: -40, width: 50, height: 50, file: 'images/flux_icons/abyssal.svg'
+    embed.svg key: ':O:', dy: -40, width: 50, height: 50, file: 'images/flux_icons/clockwork.svg'
+    embed.svg key: ':N:', dy: -40, width: 50, height: 50, file: 'images/flux_icons/neutral.svg'
+    embed.svg key: ':#N:', dy: -40, width: 50, height: 50, file: 'images/flux_icons/neutral_blank.svg'
+  end
   text str: "art", layout: :art
+
   svg file: data['art'], placeholder: "images/lab_coat.svg", layout: :art
   text str: combine_text(data['type'], data['class'], ' - '), layout: :type
-  text str: data['desc'], layout: :description
+  text(str: data['desc'], layout: :description) do | embed |
+    embed.svg key: ':exhaust:', dy: -50, width: 75, height: 75, file: 'images/icons/exhaust.svg'
+    embed.svg key: ':B:', dy: -50, width: 75, height: 75, file: 'images/flux_icons/foundation.svg'
+    embed.svg key: ':R:', dy: -50, width: 75, height: 75, file: 'images/flux_icons/gilded.svg'
+    embed.svg key: ':W:', dy: -50, width: 75, height: 75, file: 'images/flux_icons/veil_walkers.svg'
+    embed.svg key: ':G:', dy: -50, width: 75, height: 75, file: 'images/flux_icons/fey.svg'
+    embed.svg key: ':P:', dy: -50, width: 75, height: 75, file: 'images/flux_icons/abyssal.svg'
+    embed.svg key: ':O:', dy: -50, width: 75, height: 75, file: 'images/flux_icons/clockwork.svg'
+    embed.svg key: ':N:', dy: -50, width: 75, height: 75, file: 'images/flux_icons/neutral.svg'
+    embed.svg key: ':#N:', dy: -50, width: 75, height: 75, file: 'images/flux_icons/neutral_blank.svg'
+  end
+
   text str: data['attack'], layout: :attack
   text str: data['influence'], layout: :influence
   text str: data['health'], layout: :health
