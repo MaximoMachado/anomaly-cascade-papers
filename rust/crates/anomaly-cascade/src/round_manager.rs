563909db19359077d2e0cc9d5abe139d141aab47
Maximo Machado <maximo@mit.edu>
2025-02-01 23:52:59 -0800

    Redo roundmanager impl to use current round and next round for simpler logic

--- rust/crates/anomaly-cascade/src/round_manager.rs
+++ rust/crates/anomaly-cascade/src/round_manager.rs
@@ -1,10 +1,11 @@
 use crate::follower;
 use serde::{Deserialize, Serialize};
+use std::collections::VecDeque;
 
 #[derive(Clone, Default, PartialEq, Eq, Hash, Debug, Serialize, Deserialize)]
 pub struct RoundManager {
-    follower_order: Vec<follower::Id>,
-    current_position: usize,
+    current_round: Vec<follower::Id>,
+    next_round: Vec<follower::Id>,
 }
 
 impl RoundManager {
@@ -13,52 +14,47 @@ impl RoundManager {
     }
 
     fn start_new_round(&mut self) {
-        self.current_position = 0;
+        self.current_round = self.next_round.clone();
+        self.next_round = vec![];
     }
 
-    fn add_to_front(&mut self, id: follower::Id) {
-        self.follower_order.insert(0, id);
+    fn push_front(&mut self, id: follower::Id) {
+        self.current_round.insert(0, id);
     }
 
-    fn add_to_back(&mut self, id: follower::Id) {
-        self.follower_order.push(id);
+    fn push_back(&mut self, id: follower::Id) {
+        self.current_round.push(id);
     }
 
     fn remove(&mut self, id: follower::Id) -> Option<usize> {
-        let found = self.follower_order.iter().position(|el| *el == id);
+        let found = self.current_round.iter().position(|el| *el == id);
         match found {
             Some(position) => {
-                if position <= self.current_position {
-                    self.current_position -= 1;
-                }
-                self.follower_order.remove(position);
+                self.current_round.remove(position);
                 Some(position)
             }
-            None => None,
-        }
-    }
-
-    fn current(&self) -> Option<follower::Id> {
-        let curr_follower = self.follower_order.get(self.current_position);
-        match curr_follower {
-            Some(id) => Some(*id),
-            None => None,
+            None => {
+                let found = self.next_round.iter().position(|el| *el == id);
+                match found {
+                    Some(position) => {
+                        self.next_round.remove(position);
+                        Some(position)
+                    }
+                    None => None,
+                }
+            }
         }
     }
 
     fn next(&mut self) -> Option<follower::Id> {
-        let curr_follower = self.follower_order.get(self.current_position);
-        match curr_follower {
-            Some(id) => {
-                self.current_position += 1;
-                Some(*id)
-            }
-            None => None,
-        }
+        let follower = self.current_round.pop()?;
+        self.next_round.push(follower);
+
+        Some(follower)
     }
 
     fn round_over(&self) -> bool {
-        self.current_position == self.follower_order.len()
+        self.current_round.is_empty()
     }
 }
 
@@ -67,7 +63,7 @@ impl IntoIterator for RoundManager {
     type IntoIter = std::vec::IntoIter<Self::Item>;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.follower_order.into_iter()
+        self.current_round.into_iter()
     }
 }
 
@@ -76,7 +72,7 @@ impl<'a> IntoIterator for &'a RoundManager {
     type IntoIter = std::slice::Iter<'a, follower::Id>;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.follower_order.iter()
+        self.current_round.iter()
     }
 }
 
@@ -85,6 +81,6 @@ impl<'a> IntoIterator for &'a mut RoundManager {
     type IntoIter = std::slice::IterMut<'a, follower::Id>;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.follower_order.iter_mut()
+        self.current_round.iter_mut()
     }
 }
